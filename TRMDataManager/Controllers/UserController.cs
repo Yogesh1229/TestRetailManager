f1885a87936007b9c4e3333d06055ed75628d226
Yogesh505 <43180770+Yogesh505@users.noreply.github.com>
2020-12-12 19:52:34 +0530

    Get User Data - First Part. Add new API endpoint for User by adding new controller (User Controller). Add first new stored procedure for User. Add a class library project.

---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
